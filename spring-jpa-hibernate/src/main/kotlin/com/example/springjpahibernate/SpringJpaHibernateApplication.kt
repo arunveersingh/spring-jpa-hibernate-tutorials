98b521f93b5ee4d383cc040861b9668e5d536a4b
SINGH Arunveer <arunveer.singh@soprasteria.com>
2019-05-24 19:03:42 +0530

    Initial Blank Structure Commit

---
+++ spring-jpa-hibernate/src/main/kotlin/com/example/springjpahibernate/SpringJpaHibernateApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springjpahibernate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJpaHibernateApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringJpaHibernateApplication>(*args)
+}
